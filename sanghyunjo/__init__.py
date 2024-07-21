fc7c3bb49be1c34236ee5a14aa8eb78aa0ac912e
OFRIN <josanghyeokn@gmail.com>
2024-07-21 19:33:32 +0900

    import ai modules

--- sanghyunjo/__init__.py
+++ sanghyunjo/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (C) 2024 * Ltd. All rights reserved.
 # author: Sanghyun Jo <shjo.april@gmail.com>
 
-__version__ = '1.5.3'
+__version__ = '1.5.4'
 
 from .misc import *
 from .cv_utils import *
 from .json_utils import *
 from .xml_utils import *
+from .ai_utils import *
